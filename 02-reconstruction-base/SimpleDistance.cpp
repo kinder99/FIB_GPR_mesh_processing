67ca12a93f108d2d2512fcd658146a4d9434e532
kinder99 <lemouelkieran@gmail.com>
2025-11-13 11:45:51 +0100

    implemented simple reconstruction

--- 02-reconstruction-base/SimpleDistance.cpp
+++ 02-reconstruction-base/SimpleDistance.cpp
@@ -1,4 +1,5 @@
 #include "SimpleDistance.h"
+#include <iostream>
 
 
 /* Initialize everything to be able to compute the implicit distance of [Hoppe92] 
@@ -20,8 +21,26 @@ void SimpleDistance::init(const PointCloud *pointCloud, float samplingRadius)
 
 bool SimpleDistance::operator()(const glm::vec3 &P, float &value) const
 {
-	// TODO
+	std::vector<size_t> neighbors;
+	std::vector<float> sqDists;
+	knn.getKNearestNeighbors(P,1,neighbors,sqDists);
+	//std::cout << "index: " << index << std::endl;
 
+	std::vector<glm::vec3> points = cloud->getPoints();
+	std::vector<glm::vec3> normals = cloud->getNormals();
+
+	glm::vec3 pi = points.at(neighbors.at(0));
+	//std::cout << "point index ok" << std::endl;
+	glm::vec3 ni = normals.at(neighbors.at(0));
+	//std::cout << "normal index ok" << std::endl;
+
+	//projection of p onto tangent plane at pi
+	glm::vec3 z = pi - ((P - pi)*ni)*ni;
+
+	if(glm::distance(z,pi)<=this->radius){
+		value = glm::dot((P - pi),ni);
+		return true;
+	}
 	return false;
 }
 
